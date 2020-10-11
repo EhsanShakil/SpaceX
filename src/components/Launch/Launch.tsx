ed3d74b763e4ef952480a167f265c43fcda20b91
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-10-11 22:08:39 +0500

    Update Launch.tsx

--- src/components/Launch/Launch.tsx
+++ src/components/Launch/Launch.tsx
@@ -9,10 +9,9 @@ const Launch: React.FC<Props> = ({ data }: any) => {
   return (
     <div>
       <h1>SpaceX data</h1>
-      <div className='container'>
       {data.launches?.map((details: any, id: any) => {
         return (
-          <div key={id} className='data'>
+          <div key={id} className='container'>
             <h3>Flight Number: {details?.flight_number}</h3>
             <h3>Mission Name: {details?.mission_name}</h3>
             <h3>Launch Year: {details?.launch_year}</h3>
@@ -20,7 +19,7 @@ const Launch: React.FC<Props> = ({ data }: any) => {
         );
       })}
         </div>
-    </div>
+
   );
 };
 export default Launch;
