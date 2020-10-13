62c769141b9a7166ab4b87fe7563b5505a6a8a4d
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-10-13 22:52:49 +0500

    Update index.tsx

--- src/components/Launch/index.tsx
+++ src/components/Launch/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useLaunchesQuery } from "../../generated/graphql";
 import Launch from "./Launch";
+import OwnProps from './Launch'
 
 const LaunchContainer = () => {
   const { loading, error, data } = useLaunchesQuery();
@@ -8,7 +9,7 @@ const LaunchContainer = () => {
   if (loading) return <h1>Loading...</h1>;
   if (error || !data) return <h1>Error data not found</h1>;
 
-  return <Launch data={data} />;
+  return <Launch data={data} {...props} />;
 };
 
 export default LaunchContainer;
