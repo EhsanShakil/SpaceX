176c9986d449c717165d6d61ab898bdc57e06c2b
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-10-14 22:04:42 +0500

    Updating

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, {useState, useCallback} from "react";
 import Launch from "./components/Launch";
 import LaunchDetails from './components/LaunchDetails'
 
-
 const App = () => {
   let [id, setId] = useState<number>(42)
   let handleChangeId = useCallback(
--- src/components/Launch/Launch.tsx
+++ src/components/Launch/Launch.tsx
@@ -5,37 +5,39 @@ import "./style.css";
 export interface OwnProps {
   handleChangeId: (newId: number) => void;
 }
-interface Props extends OwnProps{
+interface Props extends OwnProps {
   data: LaunchesQuery;
 }
 const Launch: React.FC<Props> = ({ data, handleChangeId }) => {
   return (
     <div>
-      <h1>SpaceX data</h1>
-      {data.launches?.map((details: any, id: any) => {
-        return (
-          <div
-            key={id}
-            className="container"
-            onClick={() => handleChangeId(details.flight_number!)}
-          >
-            <div>
-              {details?.links.flickr_images.map((image: any, index: any) => (
-                <img
-                  key={index}
-                  src={image}
-                  alt="spacex"
-                  width="100px"
-                  height="100px"
-                />
-              ))}
+      <h1 style={{display: 'flex', justifyContent:'center'}}>SpaceX data</h1>
+      <div className="header">
+        {data.launches?.map((details: any, id: any) => {
+          return (
+            <div
+              key={id}
+              className="container"
+              onClick={() => handleChangeId(details.flight_number!)}
+            >
+              <div>
+                {details?.links.flickr_images.map((image: any, index: any) => (
+                  <img
+                    key={index}
+                    src={image}
+                    alt="spacex"
+                    width="100px"
+                    height="100px"
+                  />
+                ))}
+              </div>
+              <h3>Flight Number: {details?.flight_number}</h3>
+              <h3>Mission Name: {details?.mission_name}</h3>
+              <h3>Launch Year: {details?.launch_year}</h3>
             </div>
-            <h3>Flight Number: {details?.flight_number}</h3>
-            <h3>Mission Name: {details?.mission_name}</h3>
-            <h3>Launch Year: {details?.launch_year}</h3>
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 };
