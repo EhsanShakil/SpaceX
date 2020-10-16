d28701f3f5746b207a8af30732e8d1454d79ff06
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-10-16 20:43:23 +0500

    Update LaunchDetails.tsx

--- src/components/LaunchDetails/LaunchDetails.tsx
+++ src/components/LaunchDetails/LaunchDetails.tsx
@@ -10,7 +10,10 @@ const LaunchDetails: React.FC<Props> = ({ data }) => {
     return (
         <div className='launchesDetails'>
             <h1>Mission Name: {data.launch?.mission_name}</h1>
-            <h1>Mission Name: {data.launch?.launch_site?.site_name}</h1>
+            <h1>Launch Site: {data.launch?.launch_site?.site_name}</h1>
+            <h1>Launch Year: {data.launch?.launch_year}</h1>
+            <h1>Launch Year: {data.launch?.launch_success}</h1>
+            <h1>Rocket Name: {data.launch?.rocket?.rocket_name}</h1>
 
             {console.log(data)}
         </div>
